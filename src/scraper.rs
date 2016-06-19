c8413e21e3aeccc493a5e0a0139ec56a897c04f0
Adam Perry <adam.n.perry@gmail.com>
2016-06-18 23:22:04 -0700

    Scraping multiple repositories.
    Closes #38.

--- src/scraper.rs
+++ src/scraper.rs
@@ -1,76 +1,99 @@
 use std::thread;
 use std::time::Duration;
 
+use crossbeam::scope;
+
 use config::CONFIG;
 use github;
 use releases;
 use buildbot;
 
 pub fn start_scraping() {
-    let mut handles = Vec::new();
+    let orgs = ["rust-lang", "rust-lang-nursery", "rust-lang-deprecated"];
 
     // spawn the github scraper
-    handles.push(thread::spawn(|| {
-        let repo = "rust-lang/rust";
-        let sleep_duration = Duration::from_secs(CONFIG.github_interval_mins * 60);
-        loop {
-            if let Ok(gh_most_recent) = github::most_recent_update(repo) {
-                info!("scraping github activity since {:?}", gh_most_recent);
+    scope(|scope| {
+        scope.spawn(|| {
+            let sleep_duration = Duration::from_secs(CONFIG.github_interval_mins * 60);
+            'outer: loop {
+                let mut repos = Vec::new();
 
-                match github::ingest_since(repo, gh_most_recent) {
-                    Ok(()) => info!("scraped github successfully"),
-                    Err(why) => error!("unable to scrape github: {:?}", why),
+                for org in &orgs {
+                    let r = github::GH.org_repos(org);
+
+                    match r {
+                        Ok(r) => repos.extend(r),
+                        Err(why) => {
+                            error!("Unable to retrieve repos for {}: {:?}", org, why);
+                            info!("Sleeping for {} minutes", CONFIG.github_interval_mins);
+                            thread::sleep(sleep_duration);
+                            continue 'outer;
+                        }
+                    }
                 }
-            } else {
-                error!("Unable to determine most recent github update.");
-            }
 
-            info!("GitHub scraper sleeping for {} seconds ({} minutes)",
-                  sleep_duration.as_secs(),
-                  CONFIG.github_interval_mins);
-            thread::sleep(sleep_duration);
-        }
-    }));
 
-    // spawn the nightly release scraper
-    handles.push(thread::spawn(|| {
-        let sleep_duration = Duration::from_secs(CONFIG.release_interval_mins * 60);
-        loop {
-            if let Ok(rel_most_recent) = releases::most_recent_update() {
-                info!("scraping release activity since {:?}", rel_most_recent);
-                match releases::ingest_releases_since(rel_most_recent) {
-                    Ok(()) => info!("scraped releases successfully"),
-                    Err(why) => error!("unable to scrape releases: {:?}", why),
+                for repo in repos {
+
+                    match github::most_recent_update(&repo) {
+                        Ok(gh_most_recent) => {
+                            info!("scraping github activity since {:?}", gh_most_recent);
+
+                            match github::ingest_since(&repo, gh_most_recent) {
+                                Ok(()) => info!("scraped github successfully"),
+                                Err(why) => error!("unable to scrape github: {:?}", why),
+                            }
+                        }
+                        Err(why) => {
+                            error!("Unable to determine most recent github update ({:?}).", why)
+                        }
+                    }
                 }
-            } else {
-                error!("Unable to determine most recent release date.");
+
+
+                info!("GitHub scraper sleeping for {} seconds ({} minutes)",
+                      sleep_duration.as_secs(),
+                      CONFIG.github_interval_mins);
+                thread::sleep(sleep_duration);
             }
+        });
 
-            info!("Release scraper sleeping for {} seconds ({} minutes)",
-                  sleep_duration.as_secs(),
-                  CONFIG.release_interval_mins);
-            thread::sleep(sleep_duration);
-        }
-    }));
+        // spawn the nightly release scraper
+        scope.spawn(|| {
+            let sleep_duration = Duration::from_secs(CONFIG.release_interval_mins * 60);
+            loop {
+                if let Ok(rel_most_recent) = releases::most_recent_update() {
+                    info!("scraping release activity since {:?}", rel_most_recent);
+                    match releases::ingest_releases_since(rel_most_recent) {
+                        Ok(()) => info!("scraped releases successfully"),
+                        Err(why) => error!("unable to scrape releases: {:?}", why),
+                    }
+                } else {
+                    error!("Unable to determine most recent release date.");
+                }
 
-    // spawn the buildbot scraper
-    handles.push(thread::spawn(|| {
-        let sleep_duration = Duration::from_secs(CONFIG.buildbot_interval_mins * 60);
-        loop {
-            info!("scraping all buildbots...");
-            match buildbot::ingest() {
-                Ok(()) => info!("scraped build status successfully"),
-                Err(why) => error!("unable to scrape build status: {:?}", why),
+                info!("Release scraper sleeping for {} seconds ({} minutes)",
+                      sleep_duration.as_secs(),
+                      CONFIG.release_interval_mins);
+                thread::sleep(sleep_duration);
             }
+        });
 
-            info!("Buildbot scraper sleeping for {} seconds ({} minutes)",
-                  sleep_duration.as_secs(),
-                  CONFIG.buildbot_interval_mins);
-            thread::sleep(sleep_duration);
-        }
-    }));
+        // spawn the buildbot scraper
+        scope.spawn(|| {
+            let sleep_duration = Duration::from_secs(CONFIG.buildbot_interval_mins * 60);
+            loop {
+                info!("scraping all buildbots...");
+                match buildbot::ingest() {
+                    Ok(()) => info!("scraped build status successfully"),
+                    Err(why) => error!("unable to scrape build status: {:?}", why),
+                }
 
-    for handle in handles.into_iter() {
-        handle.join().unwrap();
-    }
+                info!("Buildbot scraper sleeping for {} seconds ({} minutes)",
+                      sleep_duration.as_secs(),
+                      CONFIG.buildbot_interval_mins);
+                thread::sleep(sleep_duration);
+            }
+        });
+    });
 }
