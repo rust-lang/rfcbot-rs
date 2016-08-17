3f9d655ab646993bf9e7ad08e1d0a3dbc5430a6d
Adam Perry <adam.n.perry@gmail.com>
2016-08-16 22:51:17 -0700

    Rearranging a little bit.

--- src/github/nag.rs
+++ src/github/nag.rs
@@ -6,6 +6,68 @@ use domain::github::{GitHubUser, Issue, IssueComment, Membership, Team};
 use domain::schema::*;
 use error::*;
 
+pub fn update_nags(mut comments: Vec<IssueComment>) -> DashResult<()> {
+
+    // make sure we process the new comments in creation order
+    comments.sort_by_key(|c| c.created_at);
+
+    // let mut changed_rfcs = BTreeSet::new();
+
+    for comment in &comments {
+
+        // attempt to parse a command out of the comment
+        if let Ok(command) = RfcBotCommand::from_str(&comment.body) {
+
+            let (is_by_subteam_member, author) = is_by_subteam_member(comment)?;
+
+            // don't accept bot commands from non-subteam members
+            if !is_by_subteam_member {
+                continue;
+            }
+
+            // TODO check the nag (fcp merge/close/postpone/cancel, concern, resolve, reviewed, f?)
+
+            // TODO if fcp merge/close/postpone/cancel, create/cancel the nag
+
+            // TODO if fcp concern, add a new concern
+
+            // TODO if fcp resolve, mark concern resolved
+
+
+        } else {
+
+            // TODO check to see if we need to complete any feedback requests
+
+        }
+    }
+
+    // TODO after processing all concerns/resolves check to see if any FCPs are changed
+
+    Ok(())
+}
+
+/// Check if an issue comment is written by a member of one of the subteams labelled on the issue.
+fn is_by_subteam_member(comment: &IssueComment) -> DashResult<(bool, GitHubUser)> {
+    let conn = &*DB_POOL.get()?;
+
+    let issue = issue::table.find(comment.fk_issue).first::<Issue>(conn)?;
+    let user = githubuser::table.find(comment.fk_user).first::<GitHubUser>(conn)?;
+
+    use domain::schema::memberships::dsl::*;
+
+    let many_to_many = memberships.filter(fk_member.eq(user.id)).load::<Membership>(&*conn)?;
+
+    for membership in many_to_many {
+        let team = teams::table.find(membership.fk_team).first::<Team>(conn)?;
+
+        if issue.labels.contains(&team.label) {
+            return Ok((true, user));
+        }
+    }
+
+    Ok((false, user))
+}
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum RfcBotCommand<'a> {
     FcpMerge,
@@ -75,68 +137,6 @@ impl<'a> RfcBotCommand<'a> {
             _ => Err(DashError::Misc)
         }
     }
-}
-
-pub fn update_nags(mut comments: Vec<IssueComment>) -> DashResult<()> {
-
-    // make sure we process the new comments in creation order
-    comments.sort_by_key(|c| c.created_at);
-
-    // let mut changed_rfcs = BTreeSet::new();
-
-    for comment in &comments {
-
-        // attempt to parse a command out of the comment
-        if let Ok(command) = RfcBotCommand::from_str(&comment.body) {
-
-            let (is_by_subteam_member, author) = is_by_subteam_member(comment)?;
-
-            // don't accept bot commands from non-subteam members
-            if !is_by_subteam_member {
-                continue;
-            }
-
-            // TODO check the nag (fcp merge/close/postpone/cancel, concern, resolve, reviewed, f?)
-
-            // TODO if fcp merge/close/postpone/cancel, create/cancel the nag
-
-            // TODO if fcp concern, add a new concern
-
-            // TODO if fcp resolve, mark concern resolved
-
-
-        } else {
-
-            // TODO check to see if we need to complete any feedback requests
-
-        }
-    }
-
-    // TODO after processing all concerns/resolves check to see if any FCPs are changed
-
-    Ok(())
-}
-
-/// Check if an issue comment is written by a member of one of the subteams labelled on the issue.
-fn is_by_subteam_member(comment: &IssueComment) -> DashResult<(bool, GitHubUser)> {
-    let conn = &*DB_POOL.get()?;
-
-    let issue = issue::table.find(comment.fk_issue).first::<Issue>(conn)?;
-    let user = githubuser::table.find(comment.fk_user).first::<GitHubUser>(conn)?;
-
-    use domain::schema::memberships::dsl::*;
-
-    let many_to_many = memberships.filter(fk_member.eq(user.id)).load::<Membership>(&*conn)?;
-
-    for membership in many_to_many {
-        let team = teams::table.find(membership.fk_team).first::<Team>(conn)?;
-
-        if issue.labels.contains(&team.label) {
-            return Ok((true, user));
-        }
-    }
-
-    Ok((false, user))
 }
 
 #[cfg(test)]
