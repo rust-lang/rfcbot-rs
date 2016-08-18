0810e83cc5582eef0b632b45635c14cd09000e20
Adam Perry <adam.n.perry@gmail.com>
2016-08-17 19:54:03 -0700

    Making model fields public, fixing field name typo.

--- src/domain/rfcbot.rs
+++ src/domain/rfcbot.rs
@@ -2,74 +2,76 @@ use diesel::{ExpressionMethods, FilterDsl, LoadDsl, Queryable, SaveChangesDsl, T
 
 use super::schema::*;
 
+// TODO setup disposition enum with fromsql/tosql impls
+
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[insertable_into(fcp_proposal)]
 pub struct NewFcpProposal<'a> {
-    fk_issue: i32,
-    fk_initiator: i32,
-    fk_initiating_comment: i32,
-    disposition: &'a str,
+    pub fk_issue: i32,
+    pub fk_initiator: i32,
+    pub fk_initiating_comment: i32,
+    pub disposition: &'a str,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Queryable)]
 #[changeset_for(fcp_proposal, treat_none_as_null="true")]
 pub struct FcpProposal {
-    id: i32,
-    fk_issue: i32,
-    fk_initiator: i32,
-    fk_initiating_comment: i32,
-    disposition: String,
+    pub id: i32,
+    pub fk_issue: i32,
+    pub fk_initiator: i32,
+    pub fk_initiating_comment: i32,
+    pub disposition: String,
 }
 
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[insertable_into(fcp_review_request)]
 pub struct NewFcpReviewRequest {
-    fk_proposal: i32,
-    fk_reviewer: i32,
-    fk_reviewed_comment: Option<i32>,
+    pub fk_proposal: i32,
+    pub fk_reviewer: i32,
+    pub fk_reviewed_comment: Option<i32>,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Queryable)]
 #[changeset_for(fcp_review_request, treat_none_as_null="true")]
 pub struct FcpReviewRequest {
-    id: i32,
-    fk_proposal: i32,
-    fk_reviewer: i32,
-    fk_reviewed_comment: Option<i32>,
+    pub id: i32,
+    pub fk_proposal: i32,
+    pub fk_reviewer: i32,
+    pub fk_reviewed_comment: Option<i32>,
 }
 
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[insertable_into(fcp_concern)]
 pub struct NewFcpConcern<'a> {
-    fk_proposal: i32,
-    fk_initiator: i32,
-    fk_resolved_comment: Option<i32>,
-    name: &'a str,
+    pub fk_proposal: i32,
+    pub fk_initiator: i32,
+    pub fk_resolved_comment: Option<i32>,
+    pub name: &'a str,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Queryable)]
 #[changeset_for(fcp_concern, treat_none_as_null="true")]
 pub struct FcpConcern {
-    id: i32,
-    fk_proposal: i32,
-    fk_initiator: i32,
-    fk_resolved_comment: Option<i32>,
-    name: String,
+    pub id: i32,
+    pub fk_proposal: i32,
+    pub fk_initiator: i32,
+    pub fk_resolved_comment: Option<i32>,
+    pub name: String,
 }
 
 #[derive(Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[insertable_into(rfc_feedback_request)]
 pub struct NewFeedbackRequest {
-    fk_iniatiator: i32,
-    fk_requested: i32,
-    fk_feedback_comment: Option<i32>,
+    pub fk_initiator: i32,
+    pub fk_requested: i32,
+    pub fk_feedback_comment: Option<i32>,
 }
 
 #[derive(Clone, Debug, Deserialize, Eq, Ord, PartialEq, PartialOrd, Queryable)]
 #[changeset_for(rfc_feedback_request, treat_none_as_null="true")]
 pub struct FeedbackRequest {
-    id: i32,
-    fk_initiator: i32,
-    fk_requested: i32,
-    fk_feedback_comment: Option<i32>,
+    pub id: i32,
+    pub fk_initiator: i32,
+    pub fk_requested: i32,
+    pub fk_feedback_comment: Option<i32>,
 }
