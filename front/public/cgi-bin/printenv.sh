9d65c779bcbf43a25fc122f55353b07b35e43649
bjorn3 <bjorn3@users.noreply.github.com>
2016-09-14 14:13:32 +0200

    Add humans.txt and cgi-bin/printenv.sh

---
+++ front/public/cgi-bin/printenv.sh
@@ -0,0 +1,9 @@
+404: Script Not Found
+===================
+
+The script you're looking for was not found OR YOU WERE TRYING TO HACK MY SITE.
+
+NICE TRY. I HAVE LOGGED YOUR IP ADDRESS AND YOU WILL BE HACKED!!1
+
+
+Please report vulnerabilities at [the issue tracker](https://github.com/dikaiosune/rust-dashboard/issues).
