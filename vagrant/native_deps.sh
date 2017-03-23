efb64ff2e2100d5bb505c7c96b1d64032f954980
Alan Du <alanhdu@gmail.com>
2017-03-23 21:56:59 +0100

    Fix vagrant setup

--- vagrant/native_deps.sh
+++ vagrant/native_deps.sh
@@ -11,8 +11,12 @@ mkdir -p /rust-dashboard/target
 chown -R vagrant:vagrant /rust-dashboard/
 
 # dependencies
-update-locale LANGE=en_US.UTF-8
+update-locale LANG=en_US.UTF-8
 locale-gen en_US.UTF-8
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
 apt-get update
 apt-get install -y \
     curl \
