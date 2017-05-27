a6e23a22992820c3a49e2f11bcb801d83bad81bb
Alan Du <alanhdu@gmail.com>
2017-05-27 15:08:53 +0100

    Update bootstrap.sql
    Without the unique indexes on issues and pullrequests, UPSERTs don't
    work, so it's impossible to scrape any data from a fresh bootstrap.

--- bootstrap.sql
+++ bootstrap.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:75ec882b2c7add04e1a79bdafa61fb3b94d926f1bd2bcbcbf743c3ef91717343
-size 110465327
+oid sha256:0625e9f587cd82eb5866559f8aa66d4f5518728bb1f4f3c7aa62ba1a24931c5e
+size 124342239
