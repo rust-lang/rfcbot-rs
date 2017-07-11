e196cf646724d83f996e13037ca4337a54582135
Alan Du <alanhdu@gmail.com>
2017-07-10 21:51:12 -0400

    Upgrade to Node 6.*
    The version of Node 4 that Ubuntu comes with is no longer supported

--- vagrant/native_deps.sh
+++ vagrant/native_deps.sh
@@ -17,6 +17,8 @@ locale-gen en_US.UTF-8
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+
 apt-get update
 apt-get install -y \
     curl \
