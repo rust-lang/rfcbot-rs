fe2b25e82a1e3deae137c8a89e51d08c985df3d1
Alan Du <alanhdu@gmail.com>
2017-07-10 21:51:00 -0400

    Upgrade to Ember 2.12

--- front/config/environment.js
+++ front/config/environment.js
@@ -4,16 +4,19 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'rust-dashboard',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     apiBaseURL: '/',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
-
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
@@ -30,7 +33,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
