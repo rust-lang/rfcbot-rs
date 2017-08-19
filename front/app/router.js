ccf495bae94d27ce37174a3ed2beffbf00994b7c
Adam Perry <adam.n.perry@gmail.com>
2017-08-19 14:24:06 -0700

    Remove hot issues page.

--- front/app/router.js
+++ front/app/router.js
@@ -11,12 +11,10 @@ Router.map(function() {
   this.route('issues', { path: '/issues' });
   this.route('nightlies', { path: '/nightlies' });
   this.route('prs', { path: '/pullrequests' });
-  this.route('builds', { path: '/builds' });
   this.route('links', { path: '/links' });
   this.route('triage', { path: '/triage' });
   this.route('fcp_user', { path: '/fcp/:username' });
   this.route('fcp', { path: '/fcp' });
-  this.route('hot-issues');
 });
 
 export default Router;
