e2fb5616bdba7f01ceb0edb2f56f059a570992b0
Adam Perry <adam.n.perry@gmail.com>
2017-08-26 18:30:17 -0700

    Consolidate HTTP server.

--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,34 @@
+use std::thread::{spawn, JoinHandle};
+
 use iron::prelude::*;
-use iron::status;
+use iron::error::HttpResult;
+use iron::{status, Listening};
+use mount::Mount;
 use router::Router;
 use serde_json::ser;
 
+use config::CONFIG;
 use error::DashError;
+use github::webhooks;
 use nag;
+
+pub fn serve() -> JoinHandle<HttpResult<Listening>> {
+    let mut mount = Mount::new();
+
+    mount.mount("/fcp/",
+                router!(
+        allfcps: get "/all" => list_fcps,
+        usernamefcps: get "/:username" => member_nags
+    ));
+
+    mount.mount("/github-webhook", router!(ghwebhook: post "/" => webhooks::handler));
+
+    // middleware goes here
+
+    let server_addr = format!("0.0.0.0:{}", CONFIG.server_port);
+    info!("Starting API server running at {}", &server_addr);
+    spawn(move || { Iron::new(mount).http(&*server_addr) })
+}
 
 pub fn list_fcps(_: &mut Request) -> IronResult<Response> {
     let nag_report = nag::all_fcps()?;
