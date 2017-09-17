c71095258d84a09d2017d223a64e1f854ed5deac
Adam Perry <adam.n.perry@gmail.com>
2017-09-16 20:11:07 -0700

    OK FINE

--- migrations/2017-09-17-025755_add_josh_triplett_to_cargo/up.sql
+++ migrations/2017-09-17-025755_add_josh_triplett_to_cargo/up.sql
@@ -2,4 +2,4 @@ INSERT INTO memberships (fk_member, fk_team)
 SELECT u.id, t.id
 FROM githubuser u, teams t
 WHERE t.ping = 'rust-lang/cargo' AND
-    u.login LIKE '_osh_riplett';
+    u.login = 'joshtriplett';
