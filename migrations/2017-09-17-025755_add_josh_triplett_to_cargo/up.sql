3ec924cb5d1e81d5be38458a0ebc9b4acfdebda0
Adam Perry <adam.n.perry@gmail.com>
2017-09-16 20:04:24 -0700

    looks like josh changed capitalization at some point

--- migrations/2017-09-17-025755_add_josh_triplett_to_cargo/up.sql
+++ migrations/2017-09-17-025755_add_josh_triplett_to_cargo/up.sql
@@ -2,4 +2,4 @@ DELETE FROM memberships m USING githubuser u, teams t
 WHERE
     m.fk_member = u.id AND
     m.fk_team = t.id AND
-    u.login = 'JoshTriplett' AND t.ping = 'rust-lang/cargo';
+    u.login = '_osh_riplett' AND t.ping = 'rust-lang/cargo';
