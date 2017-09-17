35cc4ee1e9e902233369dd186a10ac033a685363
Adam Perry <adam.n.perry@gmail.com>
2017-09-16 20:06:49 -0700

    ok i forgot a like operator

--- migrations/2017-09-17-025755_add_josh_triplett_to_cargo/down.sql
+++ migrations/2017-09-17-025755_add_josh_triplett_to_cargo/down.sql
@@ -2,4 +2,4 @@ DELETE FROM memberships m USING githubuser u, teams t
 WHERE
     m.fk_member = u.id AND
     m.fk_team = t.id AND
-    u.login = '_osh_riplett' AND t.ping = 'rust-lang/cargo';
+    u.login = 'joshtriplett' AND t.ping = 'rust-lang/cargo';
