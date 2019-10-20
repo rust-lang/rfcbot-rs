6d881784439101b1632b3ae24e2480f1b9f96d81
Adam Perry <adam.n.perry@gmail.com>
2019-10-19 20:41:37 -0700

    Control database setup output better.

--- setup-db.sh
+++ setup-db.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -xe
-
 DATABASE_URL="${DATABASE_URL:-postgres://postgres:ughfineokifitsfordebugging@localhost:54320/rfcbot}"
 
 until psql -d "$DATABASE_URL" -c '\q'; do
@@ -11,8 +9,17 @@ done
 
 >&2 echo "Postgres is up - executing commands"
 
+set -e
 diesel database setup
 diesel migration run
-psql -q -d "$DATABASE_URL" --file ./githubuser-backup.pg
+set +e
+
+sql_output="$(psql -q -d "$DATABASE_URL" --file ./githubuser-backup.pg)"
+sql_status="$?"
+
+if [ ! $sql_status ]; then
+  echo "$sql_output"
+  exit $sql_status
+fi
 
 exec "$@"
